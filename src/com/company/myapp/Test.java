e2650bdd67219241f8d0a63a0fbcd1bdee9b242a
raxar <ansar.utenov@gmail.com>
2023-11-21 19:16:09 +0600

    test

--- src/com/company/myapp/Test.java
+++ src/com/company/myapp/Test.java
@@ -1,6 +1,10 @@
 package com.company.myapp;
 
+import com.company.myapp.adapter.SqlFileSaver;
+import com.company.myapp.adapter.SqlToTxtFileAdapter;
+import com.company.myapp.adapter.TxtFileSaver;
 import com.company.myapp.application.MyApplication;
+import com.company.myapp.cars.Car;
 import com.company.myapp.controllers.CarController;
 import com.company.myapp.controllers.UserController;
 import com.company.myapp.data.DBSingleton;
@@ -10,12 +14,20 @@ import com.company.myapp.repositories.UserRepository;
 
 public class Test {
     public static void main(String[] args) {
+        SqlFileSaver sql = new SqlFileSaver();
+        sql.saveToSqlFile("test.sql", "Hello world!");
+        System.out.println(sql.loadFromSqlFile("test.sql"));
+        System.out.println("*****************");
+        TxtFileSaver txt = new TxtFileSaver();
         IDB db = DBSingleton.getInstance();
-        CarRepository carRepo = new CarRepository(db);
-        UserRepository userRepo = new UserRepository(db);
-        CarController carController = new CarController(carRepo);
-        UserController userController = new UserController(userRepo);
-        MyApplication application = new MyApplication(carController, userController);
-        application.start();
+        CarRepository repo = new CarRepository(db);
+        Car car = repo.getCar(23);
+        String carInfo = car.toString();
+        txt.saveToTxtFile("carInfo.txt", carInfo);
+        System.out.println(txt.loadFromTxtFile("carInfo.txt"));
+        System.out.println("*****************");
+        SqlToTxtFileAdapter adapter = new SqlToTxtFileAdapter(sql);
+        adapter.saveToTxtFile("carInfo.txt", "Test2");
+        System.out.println(adapter.loadFromTxtFile("carInfo.txt"));
     }
 }
